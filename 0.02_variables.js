dc75511792af8ec92a539cf560455894c39838f3
Kenn Pascascio <kenn370@gmail.com>
2017-08-16 14:09:29 -0400

    added varaibles

---
+++ 0.02_variables.js
@@ -0,0 +1,46 @@
+// Variables
+
+var name; 
+
+name = "Madyson";
+
+console.log(typeof(name));
+console.log(name);
+
+var lastName = "Ward";
+
+console.log(name.length);
+console.log(name.toUpperCase());
+
+// Challenge create 3 variables 'firstName' and 'lastName' and concat them 
+// together in the third variable called 'fullName' and print fullName to the console
+
+ // 'Jay Jackson'
+console.log(lastName);
+var firstName = 'Jay';
+lastName = 'Jackson';
+var fullName = firstName + " "+  lastName;
+console.log(lastName);
+console.log(fullName)
+
+// Silver 
+
+var myNumber = 0;
+console.log(myNumber);
+//            0       + 10 
+myNumber += 10// myNumber = myNumber + 10
+console.log(myNumber);
+
+//            10  - 1
+myNumber -= 1);
+console.log(myNumber);
+
+//             9     * 9
+myNumber = (myNumber * 9);
+console.log(myNumber);
+
+//            81     / 7   
+myNumber = (myNumber / 7);
+
+//          11.......
+console.log(myNumber);
