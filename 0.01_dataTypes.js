80d45e73e639b426db9ff22a8896b6413c8f8240
Kenn Pascascio <kenn370@gmail.com>
2017-08-16 13:12:41 -0400

    Added datatypes

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,70 @@
+// DataTypes 
+
+// Strings 
+// Strings are a collection of characters 
+//
+"What a Wonderful world";
+'Hello you guys';
+
+"it's a wonderful day"
+
+console.log('Thomas said "I think grass is green!"'.toUpperCase());
+
+
+console.log( 'Couldn\'t things get any better'.length );
+
+// Challenge create a string that has your name and use the to lowercase function 
+// to make sure each character is lower case   
+
+console.log('Kenn Pascascio'.toLowerCase());
+//           012345678910  
+console.log('jay'[0].toUpperCase());
+console.log('jay'[0].toUpperCase() + 'jay'[1] + 'jay'[2]);
+
+// Challenge uppercase the 'm' in 'Home' and put the string 
+// back together using string concatenation
+console.log('Home'[0] + 'Home'[1] + 'Home'[2].toUpperCase() + 'Home'[3] + 2);
+
+console.log(typeof("Amber"));
+
+// Numbers 
+
+console.log(typeof(2));
+
+console.log(2 + 5); // 7
+console.log(2 - 5);
+console.log(2 / 5);
+console.log(2 * 5);
+console.log(10 % 3);
+
+// Booleans 
+
+console.log(true)
+ /*
+	Conditional Operators
+
+	<   : less than
+	>	: greater than
+	<=	: less than or equal to
+	>=	: greater than or equal to
+	==	: equal to
+	===	: strictly equal to
+	!	: not
+	!=	: not equal to 
+ */
+console.log(4 == 5);
+console.log("Test".length > "ego".length);
+console.log(1 == "1");
+console.log(1 === "1");
+
+
+
+
+
+
+
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
+console.log('\n');
