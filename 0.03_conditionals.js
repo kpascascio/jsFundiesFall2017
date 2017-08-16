93014a9a1ea6b38f831a0ca7ea848c0c0b2cffeb
Kenn Pascascio <kenn370@gmail.com>
2017-08-16 15:10:06 -0400

    added conditionals halfway

---
+++ 0.03_conditionals.js
@@ -0,0 +1,17 @@
+// Conditionals
+
+/*
+	Chaining conditional operators
+	---------------------------------
+	&& 
+	||
+
+*/
+var name = "kenn";
+
+if ( "Kenn" == name ) {
+	console.log( "Hey your name is " + name);
+}else {
+	console.log("What is your name?");
+};
+// 
